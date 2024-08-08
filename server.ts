5ab9a9450ca025d54576ffbdfe2802f6282c0457
ntasha <nova.tasha@student.unimelb.edu.au>
2024-08-08 21:34:14 +1000

    Create a express and typescript server

---
+++ server.ts
@@ -0,0 +1,11 @@
+import express, { Express, Request, Response } from "express";
+
+const app: Express = express();
+const PORT = 3000;
+
+app.get("/", (req: Request, res: Response) => {
+    res.send("Express + TS Server");
+})
+
+app.listen(PORT);
+console.log(`server started at http://localhost:${PORT}`);
