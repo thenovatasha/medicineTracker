6b7cbf67623d9490d5796b8ce5c7c00b66075222
Nova Tasha <thenovatasha@gmail.com>
2024-08-31 19:21:20 +1000

    Test api routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,49 @@
-import React from "react";
+import React, { ChangeEvent } from "react";
+import { useState } from "react";
 
 const App = () => {
+    const [name, setName] = useState("");
+    const [password, setPassword] = useState("");
     async function onSearch(e: React.FormEvent) {
         e.preventDefault();
         fetch("http://localhost:3000/newUser", {
             method: "POST",
-            body: JSON.stringify({ name: "Nafisa", password: "HAHA" }),
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ name: name, password: password }),
         })
             .then((response) => response.text())
             .then((data) => console.log(data));
         console.log("Click Click");
     }
+    async function onNameChange(e: ChangeEvent<HTMLInputElement>) {
+        setName(e.target.value);
+    }
+    async function onPasswordChange(e: ChangeEvent<HTMLInputElement>) {
+        setPassword(e.target.value);
+    }
     return (
-        <form onSubmit={onSearch}>
+        <form>
             <label htmlFor="search">
-                <input type="text" name="search" id="search" />
+                <input
+                    onChange={onNameChange}
+                    type="text"
+                    name="search"
+                    id="search"
+                />
             </label>
-
             <label htmlFor="pass">
-                <input type="text" name="pass" id="pass" />
+                <input
+                    onChange={onPasswordChange}
+                    type="text"
+                    name="pass"
+                    id="pass"
+                />
             </label>
-            <button type="submit">Search</button>
+            <button onClick={onSearch} type="button">
+                Search
+            </button>
         </form>
     );
 };
--- src/server/server.ts
+++ src/server/server.ts
@@ -5,11 +5,17 @@ const app: Express = express();
 const PORT = 3000;
 
 app.use(cors());
+app.use(express.json());
 app.get("/", (req: Request, res: Response) => {
     res.send("Express + TS Server");
 });
 app.post("/newUser", (req: Request, res: Response) => {
-    createUser({ name: "Jack", password: "1435" });
+    const { name, password } = req.body;
+    console.log("Name: ", name);
+    console.log("Password: ", password);
+    createUser({ name: name, password: password });
+    res.statusCode = 202;
+    res.end("GOT");
 });
 app.listen(PORT);
 console.log(`server started at http://localhost:${PORT}`);
