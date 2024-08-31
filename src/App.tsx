da14e7322436d3954f4608da78dc410a8da0822c
Nova Tasha <thenovatasha@gmail.com>
2024-09-01 03:31:28 +1000

    Add env support

--- src/App.tsx
+++ src/App.tsx
@@ -2,48 +2,38 @@ import React, { ChangeEvent } from "react";
 import { useState } from "react";
 
 const App = () => {
-    const [name, setName] = useState("");
-    const [password, setPassword] = useState("");
-    async function onSearch(e: React.FormEvent) {
-        e.preventDefault();
-        fetch("http://localhost:3000/newuser", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({ name: name, password: password }),
-        });
-        console.log("Click Click");
-    }
-    async function onNameChange(e: ChangeEvent<HTMLInputElement>) {
-        setName(e.target.value);
-    }
-    async function onPasswordChange(e: ChangeEvent<HTMLInputElement>) {
-        setPassword(e.target.value);
-    }
-    return (
-        <form>
-            <label htmlFor="search">
-                <input
-                    onChange={onNameChange}
-                    type="text"
-                    name="search"
-                    id="search"
-                />
-            </label>
-            <label htmlFor="pass">
-                <input
-                    onChange={onPasswordChange}
-                    type="text"
-                    name="pass"
-                    id="pass"
-                />
-            </label>
-            <button onClick={onSearch} type="button">
-                Search
-            </button>
-        </form>
-    );
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  async function onSearch(e: React.FormEvent) {
+    e.preventDefault();
+    fetch("http://localhost:3000/signup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ name: name, password: password }),
+    });
+    console.log("Click Click");
+  }
+  async function onNameChange(e: ChangeEvent<HTMLInputElement>) {
+    setName(e.target.value);
+  }
+  async function onPasswordChange(e: ChangeEvent<HTMLInputElement>) {
+    setPassword(e.target.value);
+  }
+  return (
+    <form>
+      <label htmlFor="search">
+        <input onChange={onNameChange} type="text" name="search" id="search" />
+      </label>
+      <label htmlFor="pass">
+        <input onChange={onPasswordChange} type="text" name="pass" id="pass" />
+      </label>
+      <button onClick={onSearch} type="button">
+        Search
+      </button>
+    </form>
+  );
 };
 
 export default App;
--- src/server/server.ts
+++ src/server/server.ts
@@ -1,34 +1,37 @@
 import express, { Express, Request, Response } from "express";
 import cors from "cors";
+import "dotenv/config";
 import { createUser } from "../db/db";
 const app: Express = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
 
 type statusResponse = {
-    creationStatus: "success" | "failure";
+  creationStatus: "success" | "failure";
 };
 app.get("/", (req: Request, res: Response) => {
-    // in the future, check if user is authorized
-    res.send("User is authorized");
+  // in the future, check if user is authorized
+  res.send("User is authorized");
 });
 
 // create a new user
-app.post("/newuser", async (req: Request, res: Response) => {
-    const { name, password } = req.body;
-    let responseObject: statusResponse;
-    try {
-        await createUser({ username: name, password: password });
-    } catch (e) {
-        responseObject = { creationStatus: "failure" };
-        res.statusCode = 200;
-        res.json(responseObject);
-    }
-    responseObject = { creationStatus: "success" };
-    res.statusCode;
+app.post("/signup", async (req: Request, res: Response) => {
+  const { name, password } = req.body;
+  let responseObject: statusResponse;
+  // check if the user already exists
+  try {
+    await createUser({ username: name, password: password });
+  } catch (e) {
+    responseObject = { creationStatus: "failure" };
+    res.statusCode = 200;
     res.json(responseObject);
+    return;
+  }
+  responseObject = { creationStatus: "success" };
+  res.statusCode = 200;
+  res.json(responseObject);
 });
 
 app.listen(PORT);
